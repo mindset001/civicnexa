e6a307fa630e554201d8071c66a583f1ce116591
mindset001 <shittumoshoodademola@gmail.com>
2023-12-02 21:53:22 +0100

    state

--- src/components/form/personalDetails.jsx
+++ src/components/form/personalDetails.jsx
@@ -1,13 +1,14 @@
 import React, { Component, useState, useEffect } from 'react';
 import { Stepper } from 'react-form-stepper';
-// import '../../../src/';
 import './form.css'
-import NaijaState from 'naija-state-local-government';
-import Select from "react-select";
 
+import Select from "react-select";
+import NaijaStates from 'naija-state-local-government';
+
+console.log(NaijaStates.all());
 class PersonalDetails extends Component {
 
-  
+
   continue = e => {
     e.preventDefault();
     const isFirstNameValid = this.props.validateFirstName();
@@ -27,6 +28,9 @@ class PersonalDetails extends Component {
       othername,
       nin,
       phone,
+      occupation,
+      address,
+      dob,
       handleChange,
       validateFirstName,
       validateLastName,
@@ -40,15 +44,15 @@ class PersonalDetails extends Component {
       errorMessageLastName
     } = this.props;
 
-   
+
 
     return (
       <div className='form'>
         <form>
 
           <Stepper
-           steps={[{ label: 'Personal details' }, { label: 'Relationships' }, { label: 'Health Information' }, { label: 'Financial Information' }]}
-            
+            steps={[{ label: 'Personal details' }, { label: 'Relationships' }, { label: 'Health Information' }, { label: 'Financial Information' }]}
+
             activeStep={0}
             styleConfig={{
               activeBgColor: '#924FFF',
@@ -105,7 +109,7 @@ class PersonalDetails extends Component {
               <label htmlFor='phone' className='form-group__label'>
                 Date of Birth
               </label>
-              <input type='date' value={phone} name='phone' onChange={handleChange('phone')} className='form-group__input' placeholder='+234' />
+              <input type='date' value={dob} name='dob' onChange={handleChange('dob')} className='form-group__input' placeholder='' />
             </div>
 
             <div className="flex justify-between w-[50%] mt-4">
@@ -134,40 +138,119 @@ class PersonalDetails extends Component {
                 <input type="radio" name="marital" /> Divorced
               </div>
             </div>
-            {/* <div>
-              <label htmlFor="state_of_origin" className="text-[1.125rem] font-normal mb-[.5rem]">State of Origin</label>
-              <Select
-                defaulValue={selected}
-                onChange={setSelected}
-                options={options}
-                className="my-2"
-              />
-            </div> */}
+
+            <div>
+              <label htmlFor="state_of_origin" className="text-[1.125rem] font-normal mb-[.5rem]">State of Origin</label> <br />
+              <select className='form-group__input capitalize'>
+                <option value="">--Select State--</option>
+                <option>ABUJA FCT</option>
+                <option>ABIA</option>
+                <option>ADAMAWA</option>
+                <option>AKWA IBOM</option>
+                <option>ANAMBRA</option>
+                <option>BAUCHI</option>
+                <option>BAYELSA</option>
+                <option>BENUE</option>
+                <option>BORNO</option>
+                <option>CROSS RIVER</option>
+                <option>DELTA</option>
+                <option>EBONYI</option>
+                <option>EDO</option>
+                <option>EKITI</option>
+                <option>ENUGU</option>
+                <option>GOMBE</option>
+                <option>IMO</option>
+                <option>JIGAWA</option>
+                <option>KADUNA</option>
+                <option>KANO</option>
+                <option>KATSINA</option>
+                <option>KEBBI</option>
+                <option>KOGI</option>
+                <option>KWARA</option>
+                <option>LAGOS</option>
+                <option>NASSARAWA</option>
+                <option>NIGER</option>
+                <option>OGUN</option>
+                <option>ONDO</option>
+                <option>OSUN</option>
+                <option>OYO</option>
+                <option>PLATEAU</option>
+                <option>RIVERS</option>
+                <option>SOKOTO</option>
+                <option>TARABA</option>
+                <option>YOBE</option>
+                <option>ZAMFARA</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="state_of_origin" className="text-[1.125rem] font-normal mb-[.5rem]">State of Residence</label> <br />
+              <select className='form-group__input capitalize'>
+                <option value="">--Select State--</option>
+                <option>ABUJA FCT</option>
+                <option>ABIA</option>
+                <option>ADAMAWA</option>
+                <option>AKWA IBOM</option>
+                <option>ANAMBRA</option>
+                <option>BAUCHI</option>
+                <option>BAYELSA</option>
+                <option>BENUE</option>
+                <option>BORNO</option>
+                <option>CROSS RIVER</option>
+                <option>DELTA</option>
+                <option>EBONYI</option>
+                <option>EDO</option>
+                <option>EKITI</option>
+                <option>ENUGU</option>
+                <option>GOMBE</option>
+                <option>IMO</option>
+                <option>JIGAWA</option>
+                <option>KADUNA</option>
+                <option>KANO</option>
+                <option>KATSINA</option>
+                <option>KEBBI</option>
+                <option>KOGI</option>
+                <option>KWARA</option>
+                <option>LAGOS</option>
+                <option>NASSARAWA</option>
+                <option>NIGER</option>
+                <option>OGUN</option>
+                <option>ONDO</option>
+                <option>OSUN</option>
+                <option>OYO</option>
+                <option>PLATEAU</option>
+                <option>RIVERS</option>
+                <option>SOKOTO</option>
+                <option>TARABA</option>
+                <option>YOBE</option>
+                <option>ZAMFARA</option>
+              </select>
+            </div>
+
             <div className='form-group__element'>
               <label htmlFor='text' className='form-group__label'>
                 Occupation
               </label>
-              <input type='text' value={phone} name='phone' onChange={handleChange('phone')} className='form-group__input' placeholder='+234' />
+              <input type='text' value={occupation} name='occupation' onChange={handleChange('occupation')} className='form-group__input' placeholder='' />
             </div>
             <div className='form-group__element'>
               <label htmlFor='phone' className='form-group__label'>
                 Home Address
               </label>
-              <textarea type='text' value={phone} name='phone' onChange={handleChange('phone')} className='form-group__input' placeholder='+234'></textarea>
+              <textarea type='text' value={address} name='address' onChange={handleChange('address')} className='form-group__input' placeholder='+234'></textarea>
             </div>
 
             <div className='form-group__element'>
               <label htmlFor='phone' className='form-group__label'>
                 Upload Picture
               </label>
-              <input type='file' value={phone} name='phone' onChange={handleChange('phone')} className='form-group__input' placeholder='+234'/>
+              <input type='file' className='form-group__input' placeholder='+234' />
             </div>
 
           </div>
 
           <div style={{ textAlign: 'center' }}>
-            <button 
-            className='rounded-lg bg-[#924FFF] hover:bg-[#7C43D9] text-[1rem] leading-9 lg:text-[1.5rem] mt-[3rem] font-medium text-white w-[62%] ' onClick={this.continue}>Next</button>
+            <button
+              className='rounded-lg bg-[#924FFF] hover:bg-[#7C43D9] text-[1rem] leading-9 lg:text-[1.5rem] mt-[3rem] font-medium text-white w-[62%] ' onClick={this.continue}>Next</button>
           </div>
 
         </form>
